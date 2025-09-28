51a74dbb9e46ca66a9aba9bd259bf95cf0831874
Scottmorgan <22440249SMorgan4@users.noreply.github.com>
2025-09-27 17:05:45 -0700

    Fix: Handle case where history file not found

--- history.py
+++ history.py
@@ -1,6 +1,5 @@
 import csv
 from datetime import datetime
-import os
 
 
 def write_row(sources, file_name="history.csv"):
@@ -20,15 +19,18 @@ def write_row(sources, file_name="history.csv"):
 
 
 def check_if_empty(file_name):
-    with open(file_name, mode='r', newline='') as file:
-        reader = csv.reader(file)
-        try:
-            next(reader)
-            file.close()
-            return False
-        except StopIteration:
-            file.close()
-            return True
+    try:
+        with open(file_name, mode='r', newline='') as file:
+            reader = csv.reader(file)
+            try:
+                next(reader)
+                file.close()
+                return False
+            except StopIteration:
+                file.close()
+                return True
+    except FileNotFoundError:
+        return False
 
 
 def make_dict(sources):
